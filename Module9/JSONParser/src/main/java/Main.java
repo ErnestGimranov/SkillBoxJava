17715f7285bb0e7236bb8b78699d354a670d7aec
ErnestGimranov <ernst-120@mail.ru>
2020-02-14 04:17:01 +0500

    добавил чтение созданного json-файла и вывод кол-ва станций на линиях

--- Module9/JSONParser/src/main/java/Main.java
+++ Module9/JSONParser/src/main/java/Main.java
@@ -1,5 +1,7 @@
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -11,6 +13,7 @@ import java.util.List;
 public class Main {
     public static JSONObject mskMetroJson = new JSONObject();
     public static JSONObject mskStations = new JSONObject();
+    public static String pathToJSON = "data/msk.json";
 
     public static void main(String[] args) {
 
@@ -19,11 +22,25 @@ public class Main {
         mskMetroJson.put("stations", JSONStationParser.mkStationsInJSON(wikiDoc));
         mskMetroJson.put("lines", JSONLinesParser.mkLinesInJSON(wikiDoc));
         try{
-            FileWriter file = new FileWriter("data/msk.json");
+            FileWriter file = new FileWriter(pathToJSON);
             file.write(mskMetroJson.toJSONString().replace(",", ",\n"));
             file.flush();
             file.close();
         } catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        try {
+            JSONParser parser = new JSONParser();
+            JSONObject mskMetro = (JSONObject) parser.parse(getJsonFile());
+            JSONObject stationsOnLines = (JSONObject) mskMetro.get("stations");
+            stationsOnLines.keySet().forEach(s -> {
+                String line = (String) s;
+                JSONArray stations = (JSONArray) stationsOnLines.get(s);
+                System.out.println("На линии " + line + " - " + stations.size() + " станций");
+            });
+        }
+        catch (Exception ex){
             ex.printStackTrace();
         }
     }
@@ -34,6 +51,19 @@ public class Main {
             List<String> lines = Files.readAllLines(Paths.get(path));
             lines.forEach(line -> builder.append(line + "\n"));
         } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return builder.toString();
+    }
+
+    private static String getJsonFile()
+    {
+        StringBuilder builder = new StringBuilder();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(pathToJSON));
+            lines.forEach(builder::append);
+        }
+        catch (Exception ex) {
             ex.printStackTrace();
         }
         return builder.toString();
