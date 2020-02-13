c9d9a2770198603a57b8a33cd030d47ccb5e164a
ErnestGimranov <ernst-120@mail.ru>
2020-02-13 18:27:46 +0500

    раскидал все парсинги по классам

--- Module9/JSONParser/src/main/java/Main.java
+++ Module9/JSONParser/src/main/java/Main.java
@@ -1,4 +1,4 @@
-import org.json.simple.JSONArray;
+
 import org.json.simple.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -6,21 +6,11 @@ import org.jsoup.nodes.Document;
 import java.io.FileWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.List;
 
 public class Main {
     public static JSONObject mskMetroJson = new JSONObject();
     public static JSONObject mskStations = new JSONObject();
-    public static final int TBODY_IN_HTML = 3;
-    public static final int TD_FOR_STATION = 1;
-    public static final int TD_FOR_LINE = 0;
-    public static final int SPAN_FOR_LINE = 0;
-    public static final int SPAN_FOR_NAME = 1;
-    public static final int SPAN_FOR_SECOND_LINE_NMB = 2;
-    public static final int A_FOR_STATION = 0;
-    public static final String LINE_8A = "8А";
-    public static final String LINE_11 = "11";
 
     public static void main(String[] args) {
 
